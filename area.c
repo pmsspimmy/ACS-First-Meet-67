6ef66d3a57b047e8e7ba8ed9d3e846e9cc654583
pmsspimmy <pimmywilaiwan@gmail.com>
2024-07-31 14:42:00 +0700

    Create area.c

---
+++ area.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+   int n, w, l;
+
+    printf("1. Rectangle");
+    printf("2. Triangle");
+    printf("3. Cricle\n");
+
+// scanf("%d", &n);
+// if(n==1){
+//     printf("input width:");
+//     scanf("%d", &w);
+//     printf("input length:");
+//     scanf("%d", &l)
+// }
+// }
+}
