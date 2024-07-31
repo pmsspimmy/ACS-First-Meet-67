1a0093d5bbb2dccf9248e0f846a7960c48daac20
pmsspimmy <pimmywilaiwan@gmail.com>
2024-07-31 14:43:12 +0700

    Create bmi.c

---
+++ bmi.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main(){
+// while(1){
+//     printf("A");
+// }
+
+    // for (int i = 0 ; i <3  ; i++)
+    // {
+    //     for (int j = 0; j <3; j++)
+    //     {
+    //         printf ("i = %d j = %d\n", i, j);
+    //     }
+    //     printf ("\n");
+    // }
+// int n;
+// scanf("%d",&n);
+// for (int i=1; i<=12; i++)
+// {
+//     printf("%d * %d = %d\n",n, i, n*i);
+// }
+
+
+}
+
